63b356612045488900c4c25e6dece8c532ffd90d
keffri <keffri.w.neal@gmail.com>
2022-09-20 11:54:01 -0700

    added signup, login, logout

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,24 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+const signup_controller = require('../controllers/signupController');
+const login_controller = require('../controllers/loginController');
+const logout_controller = require('../controllers/logoutController');
+
+// HOME PAGE
+router.get('/', (req, res, next) => {
+  res.render('index', { title: 'Strive Talk', user: req.user });
 });
+
+// SIGN-UP
+router.get('/sign-up', signup_controller.signup_get);
+router.post('/sign-up', signup_controller.signup_post);
+
+//LOG IN
+router.get('/log-in', login_controller.login_get);
+router.post('/log-in', login_controller.login_post);
+
+//LOG OUT
+router.get('/log-out', logout_controller.logout_get);
 
 module.exports = router;
