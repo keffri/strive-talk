15f2c1147e5010b4fa9368f2d7bb32c1eb4c5b4d
keffri <keffri.w.neal@gmail.com>
2022-09-21 12:54:19 -0700

    password confirmation + character selection functionality

--- controllers/signupController.js
+++ controllers/signupController.js
@@ -1,20 +1,62 @@
 const User = require('../models/userModel');
 const bcrypt = require('bcryptjs');
+const { body, validationResult } = require('express-validator');
 
 exports.signup_get = (req, res, next) => {
   res.render('signup');
 };
 
-exports.signup_post = (req, res, next) => {
-  bcrypt.hash(req.body.password, 10, (err, hashedPassword) => {
-    new User({
-      username: req.body.username,
-      password: hashedPassword,
-    }).save((err) => {
-      if (err) {
-        return next(err);
+exports.signup_post = [
+  body('username')
+    .trim()
+    .isLength({ min: 4, max: 16 })
+    .escape()
+    .withMessage('Username must be at least 4 characters.'),
+  body('password')
+    .trim()
+    .isLength({ min: 8 })
+    .escape()
+    .withMessage('Password must be at least 8 characters.'),
+  body('confirmPassword')
+    .trim()
+    .isLength({ min: 8 })
+    .escape()
+    .withMessage('Password must be at least 8 characters.')
+    .custom(async (value, { req }) => {
+      if (value !== req.body.password) {
+        throw new Error('Passwords do not match');
       }
-      res.redirect('/');
-    });
-  });
-};
+      return true;
+    }),
+  async (req, res, next) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      // Errors are present
+      return res.render('signup', { passwordFail: 'Passwords do not match.' });
+    }
+
+    try {
+      const existingUser = await User.find({ username: req.body.username });
+      if (existingUser.length > 0) {
+        return res.render('signup', {
+          userFail: 'Username is already being used.',
+        });
+      }
+      bcrypt.hash(req.body.password, 10, (err, hashedPassword) => {
+        new User({
+          username: req.body.username,
+          password: hashedPassword,
+          character: req.body.character,
+        }).save((err) => {
+          if (err) {
+            return next(err);
+          }
+          res.redirect('/');
+        });
+      });
+    } catch (err) {
+      return next(err);
+    }
+  },
+];
