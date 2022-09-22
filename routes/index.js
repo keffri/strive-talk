5505a034f2710c0fdd5e539f08772cd45810adf8
keffri <keffri.w.neal@gmail.com>
2022-09-22 11:35:57 -0700

    access routes added

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,7 @@ const router = express.Router();
 const signup_controller = require('../controllers/signupController');
 const login_controller = require('../controllers/loginController');
 const logout_controller = require('../controllers/logoutController');
+const access_controller = require('../controllers/accessController');
 
 // HOME PAGE
 router.get('/', (req, res, next) => {
@@ -20,5 +21,13 @@ router.post('/log-in', login_controller.login_post);
 
 //LOG OUT
 router.get('/log-out', logout_controller.logout_get);
+
+// MEMBER ACCESS
+router.get('/member', access_controller.member_get);
+router.post('/member', access_controller.member_post);
+
+// ADMIN ACCESS
+router.get('/admin', access_controller.admin_get);
+router.post('/admin', access_controller.admin_post);
 
 module.exports = router;
