dd31e756585cb197b5e2ff423898b7552dfd2b8c
keffri <keffri.w.neal@gmail.com>
2022-09-23 11:32:08 -0700

    added virtual messageDate for display

--- models/messageModel.js
+++ models/messageModel.js
@@ -1,11 +1,16 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const { DateTime } = require('luxon');
 
 const MessageSchema = new Schema({
   user: { type: Schema.Types.ObjectId, ref: 'User', required: true },
   subject: { type: String, minLength: 4, maxLength: 30, required: true },
   message: { type: String, minLength: 4, maxLength: 140, required: true },
   date: { type: Date, default: Date.now, required: true },
+});
+
+MessageSchema.virtual('messageDate').get(function () {
+  return DateTime.fromJSDate(this.date).toFormat('DDDD, h:mm:ss, a');
 });
 
 module.exports = mongoose.model('Message', MessageSchema);
