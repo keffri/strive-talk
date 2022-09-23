84178624d0be3b21ac9385c22069bea4350d8b35
keffri <keffri.w.neal@gmail.com>
2022-09-23 10:48:18 -0700

    date typo fixed

--- models/messageModel.js
+++ models/messageModel.js
@@ -2,10 +2,10 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const MessageSchema = new Schema({
-  user: { type: Schema.Types.ObjectId, ref: User, required: true },
+  user: { type: Schema.Types.ObjectId, ref: 'User', required: true },
   subject: { type: String, minLength: 4, maxLength: 30, required: true },
   message: { type: String, minLength: 4, maxLength: 140, required: true },
-  date: { type: Date, default: Date.name(), required: true },
+  date: { type: Date, default: Date.now, required: true },
 });
 
 module.exports = mongoose.model('Message', MessageSchema);
