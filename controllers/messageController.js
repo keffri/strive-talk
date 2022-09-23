2679485cdb1a2276458d561524b56bd1477a76e0
keffri <keffri.w.neal@gmail.com>
2022-09-23 10:45:20 -0700

    messageController added

---
+++ controllers/messageController.js
@@ -0,0 +1,51 @@
+const Message = require('../models/messageModel');
+const { body, validationResult } = require('express-validator');
+
+exports.message_get = (req, res, next) => {
+  if (!res.locals.currentUser) {
+    return res.render('message', {
+      noUser: 'Sign up or login before creating a message.',
+    });
+  }
+  return res.render('message', { user: res.locals.currentUser });
+};
+
+exports.message_post = [
+  body('subject')
+    .trim()
+    .isLength({ min: 1 })
+    .escape()
+    .withMessage('Please enter a subject.')
+    .isLength({ max: 30 })
+    .escape()
+    .withMessage('Subject contains too many characters.'),
+  body('message')
+    .trim()
+    .isLength({ min: 1 })
+    .escape()
+    .withMessage('Please enter a message.')
+    .isLength({ max: 140 })
+    .escape()
+    .withMessage('Message contains too many characters.'),
+  (req, res, next) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.render('message', {
+        wrongLength:
+          'Please make sure your message/subject is the correct length.',
+      });
+    }
+
+    new Message({
+      user: res.locals.currentUser,
+      subject: req.body.subject,
+      message: req.body.message,
+    }).save((err) => {
+      if (err) {
+        return next(err);
+      }
+      res.redirect('/');
+    });
+  },
+];
