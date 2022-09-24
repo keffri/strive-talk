3fde47044ce76edd5c477c117934a88a42ef254a
keffri <keffri.w.neal@gmail.com>
2022-09-24 10:54:49 -0700

    changed username min length

--- models/userModel.js
+++ models/userModel.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
-  username: { type: String, minLength: 4, maxLength: 16, required: true },
+  username: { type: String, minLength: 3, maxLength: 16, required: true },
   password: { type: String, minLength: 8, required: true },
   isMember: { type: Boolean, default: false },
   isAdmin: { type: Boolean, default: false },
