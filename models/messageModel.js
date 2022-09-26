d051889c2c91878d68283283e87bbbd72be0f8fc
keffri <keffri.w.neal@gmail.com>
2022-09-26 10:35:28 -0700

    errors passed to render

--- models/messageModel.js
+++ models/messageModel.js
@@ -4,8 +4,8 @@ const { DateTime } = require('luxon');
 
 const MessageSchema = new Schema({
   user: { type: Schema.Types.ObjectId, ref: 'User', required: true },
-  subject: { type: String, minLength: 4, maxLength: 30, required: true },
-  message: { type: String, minLength: 4, maxLength: 140, required: true },
+  subject: { type: String, minLength: 1, maxLength: 30, required: true },
+  message: { type: String, minLength: 1, maxLength: 140, required: true },
   date: { type: Date, default: Date.now, required: true },
 });
 
